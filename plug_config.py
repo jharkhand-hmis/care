fc3803bc7b962997c968b7ba1e4b8772c9ea24c3
Vysakh Premkumar <84713473+tellmeY18@users.noreply.github.com>
2026-05-26 09:32:53 +0530

    Add multiple plugs to plug configuration

--- plug_config.py
+++ plug_config.py
@@ -1,6 +1,25 @@
 from plugs.manager import PlugManager
-from plugs.plug import Plug  # noqa: F401
+from plugs.plug import Plug
 
-plugs = []
+plugs = [
+    Plug(
+        name="care_state_hmis",
+        package_name="git+https://github.com/10bedicu/care_state_hmis.git",
+        version="@main",
+        configs={},
+    ),
+    Plug(
+        name="abdm",
+        package_name="git+https://github.com/10bedicu/care_abdm.git",
+        version="@production",
+        configs={},
+    ),
+    Plug(
+        name="care_scribe",
+        package_name="git+https://github.com/10bedicu/care_scribe.git",
+        version="@master",
+        configs={},
+    ),
+]
 
 manager = PlugManager(plugs)
